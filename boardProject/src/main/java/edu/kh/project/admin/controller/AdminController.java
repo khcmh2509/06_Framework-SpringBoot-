30da8331118f6940a69346112f25229ff9457503
khcmh2509 <khcmh2509@gmail.com>
2026-01-13 15:54:00 +0900

    관리자 추가

---
+++ boardProject/src/main/java/edu/kh/project/admin/controller/AdminController.java
@@ -0,0 +1,67 @@
+package edu.kh.project.admin.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import edu.kh.project.admin.model.service.AdminService;
+import edu.kh.project.member.model.dto.Member;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:5173")
+@RequestMapping("admin")
+@RequiredArgsConstructor
+@SessionAttributes({"loginMember"})
+public class AdminController {
+	
+	private final AdminService service;
+	
+	@PostMapping("login")
+	public Member login(@RequestBody Member inputMember, Model model) {
+		
+		Member loginMember = service.login(inputMember);
+		
+		if(loginMember == null) return null;
+		
+		model.addAttribute("loginMember", loginMember);
+		return loginMember;
+		
+	}
+	
+	@GetMapping("logout")
+	public ResponseEntity<String> logout(HttpSession session) {
+		// ResponseEntity 
+		// Spring에서 제공하는 Http 응답 데이터를
+		// 커스터마이징 할 수 있도록 지원하는 클래스
+		// -> Http 상태코드, 헤더, 응답 본문(body)을 모두 설정 가능
+		try {
+			session.invalidate(); // 세션 무효화 처리
+			return ResponseEntity.status(HttpStatus.OK) // 200
+					.body("로그아웃이 완료되었습니다");
+			
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR) // 500
+					.body("로그아웃 중 예외 발생 : " + e.getMessage());
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
